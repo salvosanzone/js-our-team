d1042944a19e489600a5d1b7b75f4bf25bbc1150
salvosanzone <salvosansone.ss@gmail.com>
2021-11-04 14:13:09 +0100

    costruzione array con gli oggetti

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/**
+1. Creiamo il nostro array di oggetti che      rappresentano ciascun membro del team.
+    Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+
+2. stampiamo tutte le card del nostro team.
+
+3. Utilizziamo poi gli input presenti nella pagina per  permettere all’utente di aggiungere nuovi membri del team. (se non fate tempo a realizzare questa parte va bene ugualmente e pensateci in ogni caso solo una volta stampata correttamente tutta la pagina)
+
+ */
+
+//creo un array al cui interno ci saranno tanti oggetti tanto quanti saranno gli elementi del nostro: 'our team'
+
+const ourTeam = [
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'img/wayne-barnett-founder-ceo.jpg'
+  },
+  
+]
+console.log('array contenente gli oggetti:',ourTeam);
+
+//stampo le 
+
+
